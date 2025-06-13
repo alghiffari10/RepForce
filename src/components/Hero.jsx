e6ff6bb97a79ab2792b34dba1da337eb5dce0ce1
alghiffari10 <raihanalghiffary28@gmail.com>
2025-06-13 17:49:18 +0700

    add jdocs for documentation code and readability

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 
-
+/**
+ * @components
+ * @param {Object} Hero - The components that shows Hero secrions 
+ * @returns {JSX.Elements} render jsx elements;
+ * */
 
 export default function Hero() {
   return (
@@ -11,12 +15,8 @@ export default function Hero() {
         <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Rep<span className="text-red-400">Force</span></h1>
       </div>
       <p className="text-sm md:text-xl font-light leading-relaxed">
-        <span className="text-red-400 font-medium">RepForce</span> is more than just a workout app — it’s your personal strength coach.
-        Whether you're just getting started or chasing your next PR, we’ve got your back with
-        <span className="text-red-400 font-medium"> personalized training plans</span>,
-        <span className="text-red-400 font-medium"> step-by-step exercise guides</span>, and
-        <span className="text-red-400 font-medium"> real-time progress tracking</span>.
-        Unlock your full potential, stay focused, and build the body you’ve always wanted.
+        <span className="text-red-400 font-medium">RepForce</span> is your personal strength coach, built to guide you with
+        <span className="text-red-400 font-medium"> personalized training plans.</span> Unlock your full potential, stay focused, and build the body you’ve always wanted.
         Because progress isn’t given it’s <span className="text-red-400 font-medium">earned.</span>
       </p>
       <button className="group relative overflow-hidden px-8 py-4 rounded-md border-2 border-red-400 text-red-400 transition-all duration-300">
